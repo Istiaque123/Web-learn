02422a5dddfffa55b4f0af182e269943d1159cb6
Istiaque123 <istiaqueremon@gmail.com>
2024-03-19 22:19:25 +0600

    Practice DataType

---
+++ Class 3/test.js
@@ -0,0 +1,19 @@
+const student = {
+    fullName  : "Istiaque Hossain",
+    age : 24,
+    cgpa : 3.80,
+    isPass: true
+}
+
+console.log(student.age);
+
+student["age"] = student.age+1;
+console.log(student.age);
+
+const product = {
+    productName  : "Ball Pen",
+    raiting: 4,
+    offer : 5,
+    price: 270.70
+
+}
