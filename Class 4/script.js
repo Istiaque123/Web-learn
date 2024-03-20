47e74494ab7948c21eb295f4cec70891960f074b
Istiaque123 <istiaqueremon@gmail.com>
2024-03-20 17:48:25 +0600

    variabl

---
+++ Class 4/script.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+/* this is multiple line comment like c, c++, java */
+
+// let a = 5, b = 10;
+
+// console.log("a = ", a, ", b = ", b, " = ", (a+b));
+// console.log("a/b = ", (a/b));
+// console.log("a^b = ", (a**b));
+// a+=1;
+// console.log(a);
+
+// Comparison  Operators
+let a = 5;
+let b = 10;
+
+console.log(a != b)
+console.log(a!==b)
+alert("I am still so weak..");
+
+let name = prompt("Enter Your name");
+console.log(name);
