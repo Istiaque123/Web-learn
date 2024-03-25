40d8e273704dec7ec363a4d00802d09e5429f9f6
Istiaque123 <istiaqueremon@gmail.com>
2024-03-25 18:19:19 +0600

    learn for each

--- Class 6/script.js
+++ Class 6/script.js
@@ -1,16 +1,40 @@
-let marks = [88, 66, 23, 86, 36, 90];
+// function myFunction(){
 
-console.log(marks.length);
-console.log(marks);
+//   console.log("This is really Pain");
+//   console.log("I really want to be free");
+//   console.log("I am so weak");
+// }
 
-marks[4] = 53;
-console.log(marks);
+// // myFunction();
 
-for (const i of marks) {
-  console.log(i);
-}
-marks.push(45);
-marks.push(75);
-console.log(marks);
-console.log(marks.slice(1, 4));
+// function funcTwo(msg) {
+//   console.log(msg);
+// }
 
+// // funcTwo("When i will be free");
+// // myFunction();
+// function sum(a, b) {
+  
+//   return a+b; 
+// }
+// // console.log(sum(73, 8));
+
+// const val = (a, b) => {
+
+//   console.log(a*b);
+// }
+
+// val(45, 6);
+
+let array = [1, 2, 3 ,4, 5, 6 ,7];
+
+array.forEach((i, ind) => {
+  console.log(ind, i*i);
+});
+
+let evenArray = array.filter((i) => {
+
+  return i%2=== 0;
+});
+
+console.log(evenArray);
