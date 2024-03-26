39d5d431080e7c4f122a3daabc4dd3cf8a81aa74
Istiaque123 <istiaqueremon@gmail.com>
2024-03-26 22:45:01 +0600

    Dom capter 2

--- Class 7/script.js
+++ Class 7/script.js
@@ -6,8 +6,11 @@
 
 let divs = document.querySelectorAll(".box");
 // console.log(divs[0 ]);
-let i = 1;
-for (const div of divs) {
-    div.innerText = `new value ${i++}`;
-}
+console.log(divs[0].getAttribute("class"));
+console.log(divs[0].getAttribute("id"));
 
+divs[1].setAttribute("class", "newBox");
+divs[1].style.backgroundColor = "yellow"
+
+divs[0].style.fontSize = "28px";
+divs[0].innerText = "Yo!!"
